580db6b4a8823b7f1c79f9b4a034f20fcd35ba5c
qwame2 <papaqwame25@gmail.com>
2026-07-30 17:38:15 -0700

    Update project

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -451,6 +451,13 @@
                         </span>
                     </a>
                 </li>
+
+                <li class="nav-item">
+                    <a href="{{ route('stores.sra-vault') }}" class="nav-link {{ request()->routeIs('stores.sra-vault') ? 'active' : '' }}" data-tooltip="Approved SRA Vault">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-archive"><rect width="20" height="5" x="2" y="3" rx="1"/><path d="M4 8v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8"/><path d="M10 12h4"/></svg>
+                        <span>Approved SRA Vault</span>
+                    </a>
+                </li>
                 @endif
 
                 @if(auth()->user()->role === 'Requisitioner')
